4178e8699db9a157d16dea102ae499214ea59a86
snowoo-oo <visual-studio@kakao.com>
2022-04-26 16:00:38 +0900

    asp.net MVC

--- asp.net/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
+++ asp.net/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -12,11 +12,12 @@ namespace MvcMovie.Controllers
         }
 
 
-        //GET: /HelloWorld/Welcome/
-        //Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+        public IActionResult Welcome(string name, int numTimes = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            ViewData["Message"] = "Hello + " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
