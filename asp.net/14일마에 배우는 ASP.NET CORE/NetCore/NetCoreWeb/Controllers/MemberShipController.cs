def678068cc3116e7c300884d34aeb8ad830600d
snowoo-oo <visual-studio@kakao.com>
2022-05-03 20:45:12 +0900

    .net core mvc / login

---
+++ asp.net/14일마에 배우는 ASP.NET CORE/NetCore/NetCoreWeb/Controllers/MemberShipController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreWeb.Models;
+
+namespace NetCoreWeb.Controllers
+{
+    public class MemberShipController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Login(LoginInfo login)
+        {
+            string message = string.Empty;
+            if (ModelState.IsValid)
+            {
+                string userId = "aaaaaa";
+                string password = "123456";
+                if(login.UserId.Equals(userId) && login.Password.Equals(password)){
+                    TempData["Message"] = "로그인이 성공적으로 이루어졌습니다.";
+                    return RedirectToAction("Index", "Membership");
+                }
+                else
+                {
+                    message = "로그인되지 않았습니다.";
+                }
+            }
+            else
+            {
+                message = "로그인정보를 올바르게 입력하세요";
+            }
+            ModelState.AddModelError(string.Empty, message);
+            return View(login);
+        }
+    }
+}
---
+++ asp.net/14일마에 배우는 ASP.NET CORE/NetCore/NetCoreWeb/Models/LoginInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreWeb.Models
+{
+    public class LoginInfo
+    {
+
+        [Required(ErrorMessage ="사용자 아이디를 입력하세요.")]
+        [MinLength(6, ErrorMessage ="사용자 아이디를 최소 6자 이상 입력하세요.")]
+        [Display(Name="사용자 아이디")]
+        public string UserId { get; set; }
+
+        [Required(ErrorMessage ="비밀번호를 입력하세요.")]
+        [MinLength(6, ErrorMessage = "비빌번호를 최소 6자 이상 입력하세요.")]
+        [Display(Name="비밀번호")]
+        public string Password { get; set; }
+    }
+}
