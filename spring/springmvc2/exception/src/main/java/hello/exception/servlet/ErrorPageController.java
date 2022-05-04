e5223b7b03c60f38ad2a3e752ca49c7f2ae694d6
snowoo-oo <visual-studio@kakao.com>
2022-05-04 10:12:36 +0900

    spring exceptionhandler, restcontrolleradvice

--- spring/springmvc2/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ spring/springmvc2/exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,6 +1,9 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,10 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION_TYPE;
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.servlet.RequestDispatcher.*;
 
 @Controller
 @Slf4j
@@ -26,6 +32,20 @@ public class ErrorPageController {
         log.info("errorPage 500");
         printErrorInfo(request);
         return "/error-page/500";
+    }
+
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest request, HttpServletResponse response){
+        log.info(("api error page 500"));
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+
+        return new ResponseEntity<>(result, HttpStatus.valueOf(statusCode));
     }
 
     private void printErrorInfo(HttpServletRequest request) {
