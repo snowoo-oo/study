5543dcfcd659f5ca50f3d89783d7491f801b95b5
snowoo-oo <visual-studio@kakao.com>
2022-05-05 17:20:39 +0900

    convert & formatter

---
+++ spring/springmvc2/typeconverter/src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
@@ -0,0 +1,18 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.convert.converter.Converter;
+
+@Slf4j
+public class IpPortToStringConverter implements Converter<IpPort, String> {
+
+    @Override
+    public String convert(IpPort source) {
+        log.info("convert source = {}", source);
+        String ip = source.getIp();
+        String port = String.valueOf(source.getPort());
+
+        return ip + ":" + port;
+    }
+}
---
+++ spring/springmvc2/typeconverter/src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,29 @@
+package hello.typeconverter.converter;
+
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService(){
+
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+
+        Assertions.assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        Assertions.assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+        Assertions.assertThat(conversionService.convert("127.0.0.1:8000", IpPort.class)).isEqualTo(new IpPort("127.0.0.1",8000));
+        Assertions.assertThat(conversionService.convert(new IpPort("127.0.0.1",8000), String.class)).isEqualTo("127.0.0.1:8000");
+
+
+    }
+}
