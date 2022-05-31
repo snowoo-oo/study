a04b73361236eb256f4433024753d093a16fe89b
snowoo-oo <visual-studio@kakao.com>
2022-05-31 13:08:04 +0900

    react details

--- react/fastcampus/part1. react 기초/ch02 react 공식문서로 디테일잡기/my-app/src/component/2-10.Form/ControlledComponent.jsx
+++ react/fastcampus/part1. react 기초/ch02 react 공식문서로 디테일잡기/my-app/src/component/2-10.Form/ControlledComponent.jsx
@@ -8,15 +8,21 @@ export default function ControlledComponent() {
     );
 
     function handleChange(event) {
-        setName(event.target.value);
+        const name = event.target.name;
+        if(name==='name')
+            setName(event.target.value);
+        if(name==='flavor')
+            setFlavor(event.target.value);
+        if(name==='essay')
+            setEssay(event.target.value);
     }
-    function handleFlavorChange(event) {
-        setFlavor(event.target.value);
-    }
+    // function handleFlavorChange(event) {
+    //     setFlavor(event.target.value);
+    // }
 
-    function handleEssayChange(event) {
-        setEssay(event.target.value);
-    }
+    // function handleEssayChange(event) {
+    //     setEssay(event.target.value);
+    // }
 
     function handleSubmit(event) {
         alert(`name: ${name}\nessay: ${essay}\nflavor: ${flavor}`);
@@ -27,18 +33,18 @@ export default function ControlledComponent() {
         <form onSubmit={handleSubmit}>
             <label>
                 Name:
-                <input type="text" value={name} onChange={handleChange} />
+                <input name="name" type="text" value={name} onChange={handleChange} />
             </label>
             <br />
             <br />
             <label>
                 Essay:
-                <textarea value={essay} onChange={handleEssayChange} />
+                <textarea name="essay" value={essay} onChange={handleChange} />
             </label>
             <br /><br />
             <label>
                 Flavor:
-                <select value="{flavor}" onChange={handleFlavorChange}>
+                <select name="flavor" value="{flavor}" onChange={handleChange}>
                     <option value="grapefruit">Grapefruit</option>
                     <option value="lime">Lime</option>
                     <option value="apple">Apple</option>
